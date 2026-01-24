60d7c0760cc31fa08e52d2883f2d559650baafcb
Rena3001 <verdiyevarana@gmail.com>
2026-01-24 19:17:50 +0400

    mobil uchun settings yazildi

--- resources/views/client/layout/includes/mobile-nav__wrapper.blade.php
+++ resources/views/client/layout/includes/mobile-nav__wrapper.blade.php
@@ -6,7 +6,7 @@
             <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
 
             <div class="logo-box">
-                <a href="index-main.html" aria-label="logo image"><img src="assets/images/resources/mobilemenu_logo.png" width="155" alt=""></a>
+                <a href="/{{ app()->getLocale() }}" aria-label="logo image"><img src="{{ $settings?->logo_dark ? asset('storage/'.$settings->logo_dark) : 'https://via.placeholder.com/180x50?text=Logo' }}" width="155" alt=""></a>
             </div>
             <!-- /.logo-box -->
             <div class="mobile-nav__container"></div>
@@ -14,20 +14,24 @@
 
             <ul class="mobile-nav__contact list-unstyled">
                 <li>
-                    <i class="icon-phone-call"></i>
-                    <a href="mailto:needhelp@packageName__.com">needhelp@agriox.com</a>
+                    <i class="fa fa-envelope"></i>
+                    <a href="mailto:{{ $settings?->email ?? 'admin@admin.com' }}">{{ $settings?->email ?? 'admin@admin.com' }}</a>
                 </li>
                 <li>
-                    <i class="icon-letter"></i>
-                    <a href="tel:666-888-0000">666 888 0000</a>
+                    <i class="fa fa-phone"></i>
+                    <a href="tel:{{ $settings?->phone ?? '123456789' }}">{{ $settings?->phone ?? '123456789' }}</a>
                 </li>
             </ul><!-- /.mobile-nav__contact -->
             <div class="mobile-nav__top">
                 <div class="mobile-nav__social">
-                    <a href="#" class="fab fa-twitter"></a>
-                    <a href="#" class="fab fa-facebook-square"></a>
-                    <a href="#" class="fab fa-pinterest-p"></a>
-                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="{{ $settings?->facebook ?? '#' }}" class="fab fa-facebook-f"></a>
+                    <a href="{{ $settings?->instagram ?? '#' }}" class="fab fa-instagram"></a>
+                    <a href="{{ $settings?->linkedin ?? '#' }}" class="fab fa-linkedin-in"></a>
+                    <a href="{{ $settings?->youtube ?? '#' }}" class="fab fa-youtube"></a>
+                    <a href="{{ $settings?->tiktok ?? '#' }}" class="fab fa-tiktok"></a>
+                    <a href="{{ $settings?->telegram ?? '#' }}" class="fab fa-telegram"></a>
+                    <a href="{{ $settings?->whatsapp ?? '#' }}" class="fab fa-whatsapp"></a>
+
                 </div><!-- /.mobile-nav__social -->
             </div><!-- /.mobile-nav__top -->
         </div>
